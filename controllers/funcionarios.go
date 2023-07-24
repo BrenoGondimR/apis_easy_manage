225b8d01605ace5ad6b044267517d4758739ae06
BrenoGondimR <breno_gondim@hotmail.com>
2023-07-23 21:51:55 -0300

    create funcionario

--- controllers/funcionarios.go
+++ controllers/funcionarios.go
@@ -1,12 +1,12 @@
 package controllers
 
 import (
+	"example/web-service-gin/helpers"
 	"example/web-service-gin/models"
 	"example/web-service-gin/utils"
 	"github.com/gin-gonic/gin"
 )
 
-// CreateFuncionario recebe as informações do front-end, cria um novo funcionário e o insere no banco de dados.
 func CreateFuncionario(c *gin.Context) {
 	funcionario := models.Funcionario{}
 	// BindJSON é utilizado para decodificar o JSON recebido do front-end para a struct funcionario.
@@ -14,13 +14,21 @@ func CreateFuncionario(c *gin.Context) {
 		c.JSON(400, gin.H{"error": "Erro ao criar funcionário"})
 		return
 	}
-	// Realize aqui qualquer validação ou tratamento adicional dos dados do funcionário, se necessário.
+
+	// Fazer o hash da senha antes de armazená-la no banco de dados
+	hashSenha, err := helpers.HashPassword(funcionario.Password)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Erro ao criar funcionário"})
+		return
+	}
+	funcionario.Password = hashSenha
 
 	// Utilize a função InsertFuncionario da model para inserir o funcionário no banco de dados.
 	if err := funcionario.InsertFuncionario(utils.DB); err != nil {
 		c.JSON(400, gin.H{"error": "Erro ao criar funcionário"})
 		return
 	}
+
 	// Retorne uma resposta de sucesso ao front-end.
 	c.JSON(201, gin.H{"message": "Funcionário criado com sucesso", "funcionario": funcionario})
 }
